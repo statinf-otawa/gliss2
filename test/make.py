a4ee20435572a4aa9d902426a82b64a02766be26
casse <casse@irit.fr>
2019-03-25 14:53:07 +0100

    test: small fix in script.

--- test/make.py
+++ test/make.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
+# maat can downloaded from https://github.com/hcasse/maat
 from maat import *
 from maat import test
 from maat import ocaml
 
 # configuration
+IRG = "../irg/"
+PRINT_IRG = IRG + "print_irg"
 GEP = "../gep/gep"
 MKIRG = "../irg/mkirg"
 GLISS_ATTR = "../gep/gliss-attr"
@@ -13,7 +16,7 @@ out = directory("out")
 # simple test case
 simple = test.case("simple", deps = out)
 
-for t in ["alone.nmp", "for.nmp", "irregular.nmp", "qvm.nml"]:
+for t in ["alone.nmp", "for.nml", "irregular.nmp", "qvm.nml"]:
 	test.command(simple, t + "-test",	"../"+GEP+" ../"+t, dir = "out")
 
 
@@ -42,4 +45,21 @@ ocaml.program("out/decode", "decode/decode.ml",
 decode = test.case("decode")
 test.command(decode, "decode-test", "./out/decode")
 
+
+# definition argument tests
+defarg = test.case("defarg-test")
+tests = [
+	("arg-bad-type",	"-D N=3"),
+	("arg-bad-arg",		"-D N=x")
+]
+for (t, a) in tests:
+	test.failing_command(defarg, "%s-test" % t,  "%s %s.nml %s" % (PRINT_IRG, t, a), err = "out/%s.err" % t)
+tests = [
+	("arg-none", ""),
+	("arg-int", "-D N=1"),
+	("arg-str", "-D N=YYY")
+]
+for (t, a) in tests:
+	test.output(defarg, "%s-test" % t, "%s %s.nml %s" % (PRINT_IRG, t, a),
+		out = "out/%s.out", out_ref = "ref/%s.out" % t)
 
