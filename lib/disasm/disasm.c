eeb69d6c45e12c513cdf036b49c1ef74bdbb92a1
Benjamin Rouxel <benjamin@lexuor.net>
2024-03-09 18:43:14 +0100

    Better disassembling of ARM/Thumb code, at least works on KDBench

--- lib/disasm/disasm.c
+++ lib/disasm/disasm.c
@@ -33,6 +33,7 @@
 typedef struct list_entry_t {
 	const char *name;
 	gliss_address_t addr;
+	uint32_t size;
 	struct list_entry_t *next;
 } list_entry_t;
 
@@ -42,6 +43,7 @@ typedef struct list_entry_t {
  */
 static list_entry_t *labels = 0;
 
+static list_entry_t *extrasyms = 0;
 
 /**
  * Print list of labels.
@@ -63,7 +65,7 @@ void print_list(list_entry_t *l) {
  * @param n		Name of label.
  * @param a		Address of label.
  */
-void add_to_list(list_entry_t **m, const char *n, gliss_address_t a) {
+void add_to_list(list_entry_t **m, const char *n, gliss_address_t a, uint32_t s) {
 
 #	ifdef GLISS_PROCESS_CODE_LABEL
 		{ GLISS_PROCESS_CODE_LABEL(a); }
@@ -77,6 +79,7 @@ void add_to_list(list_entry_t **m, const char *n, gliss_address_t a) {
 	}
 	e->name = n;
 	e->addr = a;
+	e->size = s;
 	e->next = 0;
 
 	/* find the position */
@@ -117,6 +120,50 @@ int get_label_from_list(list_entry_t *m, gliss_address_t addr, const char **name
 	/* not found */
 	else {
 		*name = 0;
+		return 0;
+	}
+}
+
+uint32_t get_item_from_list(list_entry_t *m, arm_address_t addr, list_entry_t **r) {
+
+	/* find the entry */
+	list_entry_t *e = m;
+	while(e && e->addr < addr)
+		e = e->next;
+
+	/* found ? */
+	if(e && e->addr == addr) {
+		*r = e;
+		return 1;
+	}
+
+	/* not found */
+	else {
+		return 0;
+	}
+}
+
+/**
+ * Get the label name associated with an address
+ * @param	m	the sorted list to search within
+ * @para	addr	the address whose label (if any) is wanted
+ * @param	name	will point to the name if a label exists, NULL otherwise
+ * @return	0 if no label exists for the given address, non zero otherwise
+*/
+uint32_t get_size_from_list(list_entry_t *m, gliss_address_t addr) {
+
+	/* find the entry */
+	list_entry_t *e = m;
+	while(e && e->addr < addr)
+		e = e->next;
+
+	/* found ? */
+	if(e && e->addr == addr) {
+		return e->size;
+	}
+
+	/* not found */
+	else {
 		return 0;
 	}
 }
@@ -219,6 +266,7 @@ int main(int argc, char **argv) {
 	int nb_sect_disasm = 0;
 	gliss_loader_t *loader;
 	int max_size = 0, i, j;
+	int min_size = 42;
 	char *exe_path = 0;
 	gliss_inst_t *(*decode)(gliss_decoder_t *decoder, gliss_address_t address) = gliss_decode;
 	int i_sect;
@@ -274,10 +322,21 @@ int main(int argc, char **argv) {
 	for(sym_it = 0; sym_it < gliss_loader_count_syms(loader); sym_it++) {
 		gliss_loader_sym_t data;
 		gliss_loader_sym(loader, sym_it, &data);
-		if(data.type == GLISS_LOADER_SYM_CODE || data.type == GLISS_LOADER_SYM_DATA) {
+		if(data.sect != 1)
+			continue;
+
+		if(data.type == ARM_LOADER_SYM_CODE) {
 			printf("[L]");
-			if(data.sect != 0)
-				add_to_list(&labels, data.name, data.value);
+			add_to_list(&labels, data.name, data.value, data.size);
+		}
+		else if(data.type == ARM_LOADER_SYM_DATA)
+			add_to_list(&labels, data.name, data.value, data.size);
+		else if(data.type == ARM_LOADER_SYM_NO_TYPE) {
+			if(strncmp(data.name, "$", 1) == 0) {
+				add_to_list(&extrasyms, data.name, data.value, data.size);
+			}
+			else if(strcmp(data.name, "") != 0)
+				add_to_list(&labels, data.name, data.value, data.size);
 		}
 		printf("\t%20s\tvalue:%08X\tsize:%08X\tinfo:%08X\tshndx:%08X\n", data.name, data.value, data.size, data.type, data.sect);
 	}
@@ -308,6 +367,8 @@ int main(int argc, char **argv) {
 		int size = gliss_get_inst_size_from_id(i) / 8;
 		if(size > max_size)
 			max_size = size;
+		if(size < min_size)
+			min_size = size;
 	}
 
 	/* disassemble the sections */
@@ -320,27 +381,62 @@ int main(int argc, char **argv) {
 		printf("\ndisasm new section, addr=%08x, size=%08x\n", s_tab[i_sect].addr, s_tab[i_sect].size);
 
 		/* traverse all instructions */
+		list_entry_t *cur_sym=NULL;
 		while (adr_start < adr_end) {
+			list_entry_t *le=NULL, *ee=NULL;
+			get_item_from_list(labels, adr_start, &le);
+			get_item_from_list(extrasyms, adr_start, &ee);
+
+			/* display label */
+			if(le) {
+				cur_sym = le;
+				printf("\n%08x <%s> (size: %u, last addr: %8x)\n", adr_start, le->name, cur_sym->size, adr_start+cur_sym->size);
+			}
+
+			if(ee) {
+				if(strncmp(ee->name, "$t", 2) == 0) {
+					//This is the beginning of a Thumb mode.
+					state->APSR = state->APSR | 0x00000020; //put 6th bit to 1
+					gliss_set_cond_state(d, state);
+				}
+				else if(strncmp(ee->name, "$d", 2) == 0) {
+					//This is data stuffs
+					//consume data up to the end of the symbol, or up to the next symbol, or up to the next extra symbol
+					gliss_address_t stop_data_at = adr_start;
+					if(cur_sym && cur_sym->next && cur_sym->next->addr < stop_data_at)
+						stop_data_at = cur_sym->next->addr;
+					if(ee->next && ee->next->addr < stop_data_at)
+						stop_data_at = ee->next->addr;
+					if(cur_sym && cur_sym->size > 0)
+						stop_data_at = cur_sym->addr+cur_sym->size;
+
+					if(adr_start >= stop_data_at)
+						stop_data_at = adr_start+4; //avoid looping, it happened that the cur_sym->size was actually a bit short
+
+					while(adr_start < stop_data_at) {
+						printf(" %8x:\t", adr_start);
+						printf(".word %8x\n", gliss_mem_read32(gliss_get_memory(pf, 0), adr_start));
+						adr_start += 4;
+					}
+					adr_start = stop_data_at; //in case this was not aligned
+					
+					continue;
+				}
+			}
+
+			if(adr_start >= cur_sym->addr+cur_sym->size) {
+				//some weird/unused remaining bits after the symbols
+				adr_start += min_size;
+				continue;
+			}
 
 			/* disassemble instruction */
 			int size;
 			char buff[100];
 			gliss_inst_t *inst = decode(d, adr_start);
 			gliss_disasm(buff, inst);
-			const char *n;
 
-			/* display label */
-			if(get_label_from_list(labels, adr_start, &n)) {
-				printf("\n%08x <%s>\n", adr_start, n);
-				prev_addr = adr_start;
-			}
-
-			/* display the address */
-			if((prev_addr & 0xffff0000) == (adr_start & 0xffff0000))
-				printf("    %04x:\t", adr_start & 0x0000ffff);
-			else
-				printf("%08x:\t", adr_start);
-			prev_addr = adr_start;
+			printf(" %8x:\t", adr_start);
 
 			/* display the instruction bytes */
 			size = gliss_get_inst_size(inst) / 8;
